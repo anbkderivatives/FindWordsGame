4764cc907ee6e268e1233429e5071d00af5c97d3
An Bk <andi.beka89@gmail.com>
2016-11-16 04:48:30 +0200

    Add files via upload

---
+++ src/mainClass.java
@@ -0,0 +1,15 @@
+/** 
+ * @author: frcodefever
+ */
+import findthewordgame.*;
+
+public class mainClass {
+
+    public static void main(String[] args) {
+        
+        GamePlay gp = new GamePlay();
+        gp.play();
+        
+    }
+    
+}
